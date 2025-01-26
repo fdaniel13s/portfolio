6d5319aa8649561a7a9def688817bcd11e94505b
fdaniel13s <u20221c628@upc.edu.pe>
2025-01-26 12:33:30 -0500

    refactor: configuration of personal domain.

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   //Configurations to deploy to github pages
-  site: 'https://fdaniel13s.github.io',
+  site: 'https://fdaniel13s.is-a.dev',
   base: '/portfolio',
 });
